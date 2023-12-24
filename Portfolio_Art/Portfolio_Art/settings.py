46f8a1e91f8873a2791c1742e6f8475f0ce439e5
Fernando Zalazar <zalazarfernando686@outlook.es>
2023-12-23 22:05:26 -0300

    base

--- Portfolio_Art/Portfolio_Art/settings.py
+++ Portfolio_Art/Portfolio_Art/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import statistics
+from django.conf import settings
 
 from django.urls import URLPattern
 
@@ -120,8 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    URLPattern += statistics.static(BASE_DIR / 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
