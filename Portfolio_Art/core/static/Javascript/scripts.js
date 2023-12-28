6126bd01249d158998c6852ef775e21a43d1b3a8
Fernando Zalazar <zalazarfernando686@outlook.es>
2023-12-28 20:33:02 -0300

    Search by title in home

---
+++ Portfolio_Art/core/static/Javascript/scripts.js
@@ -0,0 +1,18 @@
+// En scripts.js
+function search() {
+    console.log("pruebapruebapruebapruebapruebaprueba")
+    var input = document.getElementById("search-input");
+    var filtro = input.value.toUpperCase();
+    var cards = document.getElementById("all-cards").getElementsByClassName("cards");
+
+    for (var i = 0; i < cards.length; i++) {
+        var titulo = cards[i].getElementsByTagName("h2")[0].textContent.toUpperCase();
+        var hashtag = cards[i].getElementsByClassName("cards-hashtag")[0].textContent.toUpperCase();
+
+        if (titulo.includes(filtro) || hashtag.includes(filtro)) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
