505e370b24b9715d27dc914857c0b1601c15a2c8
Fernando Zalazar <zalazarfernando686@outlook.es>
2023-12-29 20:21:12 -0300

    show posts in database

--- Portfolio_Art/core/models.py
+++ Portfolio_Art/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255)
+
+    # FK hacia sí mismo
+    follow = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+class Post(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    likes = models.IntegerField()
+    post_date = models.DateTimeField()
+
+class Hashtag(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name_hashtag = models.CharField(max_length=50)
--- Portfolio_Art/core/views.py
+++ Portfolio_Art/core/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import User, Post, Hashtag
 
 # Create your views here.
 def HomeComponent(request):
+    posting = Post.objects.select_related('user_id').prefetch_related('hashtag_set').all()
+
+
+    context = {
+        'posting' : posting
+    }
+
     return render(
-        request, 
-        "Home.html", 
-        {}
+        request= request, 
+        template_name="Home.html", 
+        context= context
         )
