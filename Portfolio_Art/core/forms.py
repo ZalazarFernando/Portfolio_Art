a548684e86a9afda108d20a7d30d95cbc5c7eac7
Fernando Zalazar <zalazarfernando686@outlook.es>
2024-01-13 22:54:11 -0300

    change view the post

--- Portfolio_Art/core/forms.py
+++ Portfolio_Art/core/forms.py
@@ -3,14 +3,14 @@ from django import forms
 
 class BoardForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Nombre de tablero', 'class':'form-control'}))
-    description = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Descripción del tablero', 'class':'form-control', 'rows':7, 'cols':45}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Descripción del tablero', 'class':'form-control', 'rows':7, 'cols':75}))
 
     class Meta:
         model = Board
         fields = ['name', 'description']
 
 class CommentForm(forms.ModelForm):
-    comment = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Escribe tu comentario', 'name':'field-comment', 'class':'form-control', 'rows':7, 'cols':75}))
+    comment = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Escribe tu comentario', 'name':'field-comment', 'class':'form-control', 'rows':7, 'cols':45}))
     
     class Meta:
         model = Comments
