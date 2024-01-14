854e497fe666213a9a1bfd94c2c1686d77b482b5
Fernando Zalazar <zalazarfernando686@outlook.es>
2024-01-13 21:19:26 -0300

    common user scripts in js file

--- Portfolio_Art/core/static/Javascript/scripts.js
+++ Portfolio_Art/core/static/Javascript/scripts.js
@@ -10,3 +10,85 @@ function handleKeyPress(event) {
         search();
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    var cards = document.querySelectorAll('.card-item');
+    var contextMenu = document.getElementById('context-menu');
+
+    cards.forEach(function (card) {
+        card.addEventListener('contextmenu', function (event) {
+            event.preventDefault();
+            var postId = card.getAttribute('data-post-id');
+            showContextMenu(event.clientX, event.clientY, postId);
+        });
+    });
+
+    function showContextMenu(x, y, postId) {
+        contextMenu.style.display = 'block';
+        contextMenu.style.left = x + 'px';
+        contextMenu.style.top = y + 'px';
+
+        // Almacena el id en un atributo de datos
+        contextMenu.setAttribute('data-post-id', postId);
+
+        // Agrega eventos
+        var btnEdit = document.getElementById('btn-edit');
+        var btnDelete = document.getElementById('btn-delete');
+
+        btnEdit.addEventListener('click', function () {
+            handleMenuAction('edit', postId);
+        });
+
+        btnDelete.addEventListener('click', function () {
+            handleMenuAction('delete', postId);
+        });
+
+        document.addEventListener('click', hideContextMenu);
+    }
+
+    function hideContextMenu() {
+        contextMenu.style.display = 'none';
+        document.removeEventListener('click', hideContextMenu);
+
+        // Elimina los eventos al ocultar el menú
+        var btnEdit = document.getElementById('btn-edit');
+        var btnDelete = document.getElementById('btn-delete');
+
+        btnEdit.removeEventListener('click', handleMenuAction);
+        btnDelete.removeEventListener('click', handleMenuAction);
+    }
+
+    function handleMenuAction(action, postId) {
+        if(postId) {
+            if(action==='edit'){
+                var editUrl = `/edit_post/${postId}/`;
+                window.location.href = editUrl;
+            } else {
+                //obtener csrfToken
+                var csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+
+                $.ajax({
+                    type: "POST",
+                    url: `/delete_post/`,
+                    data: {
+                        'post_id': postId,
+                    },
+                    headers: {
+                        'X-CSRFToken': csrfToken
+                    },
+                    success: function(response){
+                        if (response === 'success') {
+                            console.log('Post delete successfully');
+                            location.href = "";  
+                        } else {
+                            console.log('Error delete post');
+                        }
+                    },
+                    error: function(error) {
+                        console.log('Error delete post:', error);
+                    }
+                })
+            }
+        }
+    }
+});
